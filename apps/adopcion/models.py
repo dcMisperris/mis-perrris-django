3365af0a5de20cf81805c43c07bcde61093587fa
Nicolas <nicocarrrenoh@gmail.com>
2018-11-07 21:45:41 -0300

    Aplicacion Mis Perris, primer commit

---
+++ apps/adopcion/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Mascota(models.Model):
+    OPCIONES_ESTADO = (('Rescatado','Rescatado'),('Disponible','Disponible'),('Adoptado','Adoptado'),)
+    fotografia = models.ImageField(upload_to = "static/img/")
+    nombre = models.CharField(max_length = 30)
+    raza_predominante = models.CharField(max_length = 50)
+    descripcion = models.TextField()
+    estado = models.CharField(max_length = 15 , choices = OPCIONES_ESTADO,default = 'Rescatado')
+
+    def __str__(self):
+        return self.nombre
