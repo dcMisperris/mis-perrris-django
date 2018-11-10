dde92ddcc35c2dba6c269fd73d38df6c1de9792f
Nicolas <nicocarrrenoh@gmail.com>
2018-11-09 23:00:17 -0300

    mis_perris, cuarto Commi

--- apps/adopcion/views.py
+++ apps/adopcion/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html',{})
+
+def adoptar(request):
+    return render(request,'adoptar.html',{})
